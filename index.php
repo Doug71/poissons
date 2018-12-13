a2f9be796aa1d838257d025fd8453f36810d449c
NicolasWack <wack.nicolas@stagiairesifa.fr>
2018-12-13 15:04:06 +0100

    mise à jour 13/12/2018

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<section class="accueil">
 			<p>Bonjour et bienvenu sur notre catalogue en ligne de poissons. Ce catalogue a pour but de repertorier différentes espèces de poissons.</p>
 			<p>Vous pouvez dès à présent consulter les fiches via l'onglet "catalogue".<br>
-				Si vous cliquez sur les fiches vous avez accès à la description du poisson. Il est également possible de modifier ou supprimer les fiches<br>
+				Si vous cliquez sur les fiches vous avez accès à la description du poisson. Il est également possible de modifier ou supprimer les fiches.<br>
 				Si vous le souhaitez vous pouver contribuer à votre tour et créer une fiche, via l'onglet "création" du menu "catalogue", c'est très simple !</p>
 			<p>Bonne découverte !</p>
 		</section>
